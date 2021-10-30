685be28278448e1240a797f8486dbd33183f37c7
tanaya123 <tanayawankar58@gmail.com>
2021-10-30 16:45:40 +0530

    Connect to Database

---
+++ app.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const url='mongodb://localhost/sample'
+
+
+const app=express()
+
+mongoose.connect(url,{useNewUrlParser:true})
+const con=mongoose.connection
+
+con.on('open',function(){
+    console.log('connected...')
+})
