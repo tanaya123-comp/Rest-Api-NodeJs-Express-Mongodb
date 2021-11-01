4a73fd7e8e0389854bad5162eecd1113ce1f2599
tanaya123 <tanayawankar58@gmail.com>
2021-11-01 18:54:11 +0530

    Add Routes

--- routes/aliens.js
+++ routes/aliens.js
@@ -13,6 +13,18 @@ router.get('/',async(req,res)=>{
     }
 })
 
+
+router.get('/:id',async(req,res)=>{
+    try{
+        const alien=await Alien.findById(req.params.id)
+        res.json(alien);
+    }
+    catch(err)
+    {
+        console.log("Error"+err);
+    }
+})
+
 router.post('/',async(req,res)=>{
     console.log(req.body)
     const alien=new Alien({
@@ -27,6 +39,30 @@ router.post('/',async(req,res)=>{
 
     }catch(error){
         res.send(error)
+    }
+})
+
+router.patch('/:id',async(req,res)=>{
+    try{
+        const alien=await Alien.findById(req.params.id);
+        alien.sub=req.body.sub
+        const a1=await alien.save()
+        res.json(a1)
+
+    }catch(err)
+    {
+        res.send(err);
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+    try{
+        const alien=await Alien.findById(req.params.id);
+        const a1=await alien.remove()
+        res.send('removed')
+    }catch(err)
+    {
+        res.send(err)
     }
 })
 
